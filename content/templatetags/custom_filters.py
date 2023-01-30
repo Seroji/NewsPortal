ef69743dbe45d9fec5d36233182803daa8d5a029
Seroji <gamexr6@mail.ru>
2023-01-31 00:34:43 +0900

    First commit'

---
+++ content/templatetags/custom_filters.py
@@ -0,0 +1,49 @@
+from django import template
+from .banned import BANNED_WORDS
+
+register = template.Library()
+
+def type_check(value):
+    if isinstance(value, str):
+        return value
+    else:
+        raise ValueError
+
+
+def replace_with_stars(value:str):
+    _list = list(value)
+    for i in range(len(value)):
+        if _list[-1] != '*':
+            _list[i+1] = '*'
+        else:
+            break
+    return "".join(_list)
+
+
+def checking_letter(value:str, orig:list, num:int):
+    if orig[num][0].isupper():
+        changed = list(value)
+        changed[0] = changed[0].upper()
+        changed = "".join(changed)
+        return changed
+    else:
+        return value
+
+
+@register.filter()
+def censor(value):
+    try:
+        original_str = value.split(' ')
+        lower_str = value.lower().split(' ')
+        result = []
+        i = -1
+        for word in lower_str:
+            i += 1
+            if word in BANNED_WORDS:
+                word = replace_with_stars(word)
+            word = checking_letter(word, original_str, i)
+            result.append(word)
+        return " ".join(result)
+    except ValueError:
+        print("Переданная переменная не является строковой!")
+        return value
--- content/views.py
+++ content/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class NewsList(ListView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
+    ordering = '-time_in'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        return context
+
+
+class NewsDetail (DetailView):
+    model = Post
+    template_name = 'one_news.html'
+    context_object_name = "obj"
